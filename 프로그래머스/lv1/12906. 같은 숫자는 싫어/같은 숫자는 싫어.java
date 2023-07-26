95a6b953efa1b7e5424dc6e0242b1bd06b156432
Jin Sol Jeon <116371230+sorydory@users.noreply.github.com>
2023-07-26 20:01:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 54.31 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : arr) {
+            if (stack.isEmpty() || num != stack.peek()) {
+                stack.push(num);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
